46e6da2112398ebd77853779e50e4882eb4fbf85
aguz11 <agus110196@gmail.com>
2024-04-16 20:32:57 -0300

    Update EDATp32.java

--- src/eda/tp3/pkg2/EDATp32.java
+++ src/eda/tp3/pkg2/EDATp32.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package eda.tp3.pkg2;
 
 /**
@@ -10,11 +7,8 @@ package eda.tp3.pkg2;
  */
 public class EDATp32 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    
     }
     
 }
